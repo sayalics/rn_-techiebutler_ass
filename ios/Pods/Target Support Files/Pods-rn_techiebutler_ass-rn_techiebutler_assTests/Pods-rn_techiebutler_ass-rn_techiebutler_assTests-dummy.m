5201723c2be4aaa0c8c4a7f0847c97f3ff3ee742
Sayali <sayalia72@gmail.com>
2024-04-08 20:32:03 +0530

    react native project setup

---
+++ ios/Pods/Target Support Files/Pods-rn_techiebutler_ass-rn_techiebutler_assTests/Pods-rn_techiebutler_ass-rn_techiebutler_assTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rn_techiebutler_ass_rn_techiebutler_assTests : NSObject
+@end
+@implementation PodsDummy_Pods_rn_techiebutler_ass_rn_techiebutler_assTests
+@end
